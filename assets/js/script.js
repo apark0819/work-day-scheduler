109e64fdcf31b52cdd893f9bbb8182ccd93417a8
apark0819 <apark0819@gmail.com>
2022-07-01 11:02:46 -0700

    var cmt

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,17 @@
 var today = moment();
 console.log("today", today);
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+//var elements
+var saveBtn = $(".save-icon");
+var containerEl = $(".container");
+var nineAM = $("#9AM");
+var tenAM = $("#10AM");
+var elevenAM = $("#11AM");
+var twelvePM = $("#12PM");
+var onePM = $("#1PM");
+var twoPM = $("#2PM");
+var threePM = $("#3PM");
+var fourPM = $("#4PM");
+var fivePM = $("#5PM");
+
